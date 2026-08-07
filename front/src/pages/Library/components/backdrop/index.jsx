392e0fbba680905461bc179b0406c815db04adb1
Dmitriy Andreev <pro100dimka.and@gmail.com>
2026-08-07 13:39:08 +0300

    а

--- front/src/pages/Library/components/backdrop/index.jsx
+++ front/src/pages/Library/components/backdrop/index.jsx
@@ -27,14 +27,14 @@ function MusicObject({ type, parts }) {
 export default function LibraryBackdrop() {
   return (
     <div className="library-concert-backdrop" aria-hidden="true">
-      {BACKGROUNDS.map(([theme, src]) => (
+      {/* {BACKGROUNDS.map(([theme, src]) => (
         <img
           key={theme}
           className={`library-theme-space library-theme-space--${theme}`}
           src={src}
           alt=""
         />
-      ))}
+      ))} */}
       <div className="library-stage-lights">
         <i />
         <i />
